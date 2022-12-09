91d9eea90d2b103373997e848c9fd736065b14dd
OZISANPC <eiden96004@gmail.com>
2022-12-10 02:22:38 +0900

    ver 0.1 リポジトリ再作成

---
+++ CCharpStudentStudy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// エントリポイントとなるクラス
+/// </summary>
+internal class Program
+{
+    /// <summary>
+    /// エントリポイントメソッド
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("HelloWorld");
+    }
+}
