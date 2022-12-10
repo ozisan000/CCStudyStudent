34e01d57cdadae7059bc3c059a921339eac1ec34
OZISANNOTEPC <eiden96004@gmail.com>
2022-12-10 19:24:27 +0900

    ver 1.1 chapter2 C#の基本 作成開始

---
+++ CCharpStudentStudy/Chapter2BasicCCharp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCharpStudentStudy
+{
+    public class Chapter2BasicCCharp
+    {
+        public void Main()
+        {
+            //①2-1 変数を体験しよう method作成
+            VariableDeclaration();
+        }
+
+        /// <summary>
+        /// 変数宣言
+        /// </summary>
+        private void VariableDeclaration()
+        {
+            //②変数「宣言」している、箱を机の上にドカって置くイメージ,
+            //実際は変数の名前をC#に通達し値を格納するための領域をメモリ上に確保することを言う
+            int data;
+            int data1, data2;   //③変数をカンマでつなげて宣言できるがお勧めしない、原則変数をひとつづつ宣言
+
+
+        }
+    }
+}
